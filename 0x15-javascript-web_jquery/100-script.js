fc9dfcdba959e93a3c0867f05c855a23239f0005
kachinne <nobbnet@gmail.com>
2023-05-05 07:53:38 -0700

    Checking html

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const headerElement = document.querySelector('HEADER');
+   headerElement.style.color = '#FF0000';
+});
---
+++ 0x15-javascript-web_jquery/5-script.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+	$('Div#add_item').click(function (){
+		$('UL.my_list').append('<li>Item</li>');
+	});
+});
