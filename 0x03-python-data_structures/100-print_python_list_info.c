6e2734c3fc69fea4c982aaa0b11c6888a8135176
kachinne <nobbnet@gmail.com>
2023-09-04 11:02:38 -0700

    Do it well

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,27 +1,38 @@
-#include <stdio.h>
+
 #include <Python.h>
+#include <stdio.h>
 
-/**
- * print_python_list_info - prints python list info
- *
- * @p: PyObject
- * Return: no return
- */
-void print_python_list_info(PyObject *p)
-{
-	long int size, i;
-	PyListObject *list;
-	PyObject *item;
+void print_python_list_info(PyObject *p) {
+    Py_ssize_t len = PyList_Size(p);
+    Py_ssize_t i;
 
-	size = Py_SIZE(p);
-	printf("[*] Size of the Python List = %ld\n", size);
+    printf("[*] Size of the Python List = %ld\n", len);
+    printf("[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
 
-	list = (PyListObject *)p;
-	printf("[*] Allocated = %ld\n", list->allocated);
+    for (i = 0; i < len; i++) {
+        PyObject *item = PyList_GetItem(p, i);
+        const char *item_type = item->ob_type->tp_name;
 
-	for (i = 0; i < size; i++)
-	{
-		item = PyList_GetItem(p, i);
-		printf("Element %ld: %s\n", i, Py_TYPE(item)->tp_name);
-	}
+        printf("Element %ld: %s\n", i, item_type);
+    }
+}
+
+int main(int argc, char *argv[]) {
+    // This is a test program. You can call the print_python_list_info
+    // function with a Python list from your Python code.
+    // We can use it to work on C to python
+
+    Py_Initialize();
+
+    // Create a Python list (you can replace this with your own list)
+    PyObject *my_list = PyList_New(3);
+    PyList_SetItem(my_list, 0, PyLong_FromLong(1));
+    PyList_SetItem(my_list, 1, PyFloat_FromDouble(3.14));
+    PyList_SetItem(my_list, 2, PyUnicode_DecodeUTF8("Hello", 5, "strict"));
+
+    print_python_list_info(my_list);
+
+    Py_Finalize();
+
+    return 0;
 }
